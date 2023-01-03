60a76c9b27a0f86a84a5d2ea5f0deacdba93e933
elusso <enzo.lusso@hotmail.fr>
2023-01-03 14:38:29 +0100

    update

--- install.sh
+++ install.sh
@@ -4,24 +4,39 @@ distro=$(lsb_release -i -s)
 version=$(lsb_release -r -s)
 echo -e "Distribution: \e[32m$distro\e[0m"
 echo -e "Version: \e[32m$version\e[0m"
-read -n1 -p "wait..."
+
+echo -e "\e[1;34mRequired packages\e[0m"
 
 if ! which nginx > /dev/null 2>&1; then
     read -n1 -p "nginx is not present, do you want to install nginx(y/n): " install_nginx 
-    case $install_nginx in  
-    y|Y) sudo apt install nginx ;; 
-    *) echo dont know ;; 
-    esac
+    if [ "$install_nginx" == "y" ];
+    then
+    sudo apt install nginx -y
+    echo $(nginx -v)
+    fi
 else
     echo -e "nginx     \e[32mOK\e[0m"
 fi
 
 if ! which php > /dev/null 2>&1; then
     read -n1 -p "php is not present, do you want to install php(y/n): " install_php 
-    case $install_php in  
-    y|Y) sudo apt install php ;; 
-    *) echo dont know ;; 
-    esac
+    if [ "$install_nginx" == "y" ];
+    then
+    sudo apt install dirmngr ca-certificates software-properties-common gnupg gnupg2 apt-transport-https curl -y
+        if ["$distro" == "Debian"];
+        then
+        curl -sSL https://packages.sury.org/php/README.txt | sudo bash -x
+        fi
+        if ["$distro" == "Ubuntu"];
+        then
+        sudo add-apt-repository ppa:ondrej/php
+        fi
+    sudo apt update
+    sudo apt list --upgradable
+    sudo apt upgrade -y
+    sudo apt install php8.2 php8.2-cli php8.2-fpm php8.2-sqlite3 -y
+    echo $(php -v)
+    fi
 else
     echo -e "php       \e[32mOK\e[0m"
 fi
@@ -63,14 +78,13 @@ echo -e "\e[1;34mBegin configuration: \e[0m"
 read -p "Web port (80 recommanded): " webport
 read -p "hostname (Ex: localhost): " hostname
 
-read -p "MQTT Broker port (80 recommanded): " mqtt_port
 
-read -p "Do you want to create mosquitto user (y/n):" create_mqtt_user
-if [ "$create_mqtt_user" == "y" ];
-then
-pass_output=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w12 | head -n 1)
-echo -e "\e[1;31mSAVE THIS DATA FOR LATER: \e[0m"
-echo "MQTT username: homeqtt"
-echo "MQTT password: $pass_output"
+#read -p "Do you want to create mosquitto user (y/n):" create_mqtt_user
+#if [ "$create_mqtt_user" == "y" ];
+#then
+#pass_output=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w12 | head -n 1)
+#echo -e "\e[1;31mSAVE THIS DATA FOR LATER: \e[0m"
+#echo "MQTT username: homeqtt"
+#echo "MQTT password: $pass_output"
 #mosquitto_passwd -b /etc/mosquitto/passwd homeqtt 
-fi
+#fi
