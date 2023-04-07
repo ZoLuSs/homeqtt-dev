da66c75c1051b22b436c6e02a39cb44d5ec1fbfd
elusso <enzo.lusso@hotmail.fr>
2023-04-07 21:54:39 +0200

    update

--- web/config/session.php
+++ web/config/session.php
@@ -1,9 +1,20 @@
 <?php
 ini_set('session.gc_maxlifetime', 14400);
-
+session_cache_expire(14400);
 session_start();
-if(!$_SESSION["login"]){
+if(isset($_SESSION["login"])){
+    if(!$_SESSION["login"]){
+        http_response_code(401);
+        header("Location: /login");
+        exit(json_encode("You need to login for accessing this page !"));
+    }
+}
+else{
     http_response_code(401);
     header("Location: /login");
-    exit(json_encode("You need to login for accessing this page !"));
+    exit(json_encode("Session variable has been delete !"));
 }
+
+
+echo "gc_maxlifetime: " . ini_get('session.gc_maxlifetime') . " / session.cookie_lifetime: " . ini_get('session.cookie_lifetime') . " / session.gc_probability: " .  ini_get('session.gc_probability') . " / session.gc_divisor: " .  ini_get('session.gc_divisor') . " / session.save_path: " .  ini_get('session.save_path');
+echo var_dump('session');
